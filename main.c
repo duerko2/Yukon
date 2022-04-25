9dd80fe695abdccea068a803e6d83fba557b6cee
marcusjacobsen <S215799@dtu.dk>
2022-04-25 14:17:52 +0200

    LinkedList for allll

--- main.c
+++ main.c
@@ -62,6 +62,47 @@ Card * createDeck(){
 }
 
 
+// Not working atm. Can't return arrays.
+/*
+Card* dealCards(Card *deck){
+Card * column[] = {NULL,NULL,NULL,NULL,NULL,NULL,NULL};
+
+
+int columnValues[] = {1,6,7,8,9,10,11};
+
+Card * currentCard = (Card*)malloc(sizeof (Card));
+
+currentCard = deck;
+for(int i=0;i<52;i++) {
+for (int j = 0; j < 7; j++) {
+int allowedAmountOfCards = columnValues[j];
+int currentIndex = 0;
+
+
+Card *currentPlacement = column[j];
+
+
+while (currentPlacement != NULL && currentIndex < allowedAmountOfCards) {
+currentPlacement = currentPlacement->next;
+currentIndex++;
+}
+if(currentIndex<allowedAmountOfCards) {
+currentPlacement = createCard(currentCard->value, currentCard->suit);
+currentPlacement->next = column[j];
+column[j] = currentPlacement;
+
+
+currentCard = currentCard->next;
+}
+}
+}
+
+Card **arrayPointer=column;
+
+return column;
+}
+*/
+
 
 int main() {
 
@@ -106,14 +147,6 @@ for(int i=0;i<52;i++) {
 
 
 
-
-
-for(int i=0;i<7;i++){
-    column[i]=currentCard;
-    for(int j=0;j<columnValues[i];i++){
-
-    }
-}
 
 
 
